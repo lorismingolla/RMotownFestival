47b1318434b30a7ae64627470ba342a8db73f929
Loris Mingolla <loris.mingolla@student.odisee.be>
2021-03-02 16:01:44 +0100

    Update environment.prod.ts

--- frontend/RMotownFestival/src/environments/environment.prod.ts
+++ frontend/RMotownFestival/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   festivalName: "RMotownFestival",
-  apiBaseUrl: "https://brave-sea-0e7d3a503.azurestaticapps.net/api/"
+  apiBaseUrl: "https://rmotownfestivalapilm.azurewebsites.net/api/"
 };
